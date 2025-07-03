f878565e67b388c565bc9973ed09e48d74643f0a
Isaac Lea <isaac.lea@nttdata.com>
2025-07-03 09:33:46 -0700

    initial checking of spring initializr springboot app

---
+++ java/test-containers-app/src/main/java/demo_apps/java/spring/test_containers_app/TestContainersAppApplication.java
@@ -0,0 +1,13 @@
+package demo_apps.java.spring.test_containers_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainersAppApplication.class, args);
+	}
+
+}
