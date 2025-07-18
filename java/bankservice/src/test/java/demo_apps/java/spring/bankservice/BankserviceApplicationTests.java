9c52978dd4495d0c66e09093e375a6d68143f5c5
Isaac Lea <isaac.lea@nttdata.com>
2025-07-18 09:12:48 -0700

    Added test to verify application.properties in the /test folder overrides the main one during unit tests

--- java/bankservice/src/test/java/demo_apps/java/spring/bankservice/BankserviceApplicationTests.java
+++ java/bankservice/src/test/java/demo_apps/java/spring/bankservice/BankserviceApplicationTests.java
@@ -1,13 +1,21 @@
 package demo_apps.java.spring.bankservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BankserviceApplicationTests {
 
+	@Value("${spring.application.name}")
+	private String appName;
+
 	@Test
 	void contextLoads() {
+		// This test will pass if the application context loads successfully
+		// and the application name is set correctly.
+		System.out.println("Application Name: " + appName);
+		assert appName.equals("bankserviceTEST");
 	}
 
 }
